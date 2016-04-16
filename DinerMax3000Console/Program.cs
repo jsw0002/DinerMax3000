280ab2043ac575a179eb186bac694c2b1a0d31c6
Jordan Willis <jsw0002@gmail.com>
2016-04-16 15:29:00 -0400

    Everything before the database creation

--- DinerMax3000Console/Program.cs
+++ DinerMax3000Console/Program.cs
@@ -10,6 +10,42 @@ namespace DinerMax3000Console
     {
         static void Main(string[] args)
         {
+            FoodMenu summerMenu = new FoodMenu();
+            summerMenu.Name = "Summer menu";
+            summerMenu.AddMenuItem("Salmon", "Fresh Norwegian Salmon with Sandefjord butter.", 25.50);
+            summerMenu.AddMenuItem("Taco", "All Norwegians eat tacos on Fridays.", 10);
+            summerMenu.HospitalDirections = "Right around the corner of 5th street. Ask for Dr. Jones.";
+
+            DrinkMenu outsideDrinks = new DrinkMenu();
+            outsideDrinks.Disclaimer = "Do not drink and code.";
+            outsideDrinks.AddMenuItem("Virgin Cuba Libre", "A classic.", 10);
+            outsideDrinks.AddMenuItem("Screwdriver", "Makes you hammered", 15);
+
+            Order hungryGuestOrder = new Order();
+            
+            for(int x = 0; x <= summerMenu.items.Count - 1; x++ )
+            {
+                MenuItem currentItem = summerMenu.items[x];
+                hungryGuestOrder.items.Add(currentItem);
+            }
+
+            foreach (MenuItem currentItem in outsideDrinks.items)
+            {
+                hungryGuestOrder.items.Add(currentItem);
+            }
+
+            Console.WriteLine("The total is: " + hungryGuestOrder.Total);
+
+            try
+            {
+                outsideDrinks.AddMenuItem("Himkok", "9 of 10 people recommend staying away from this drink", 0);
+            }
+            catch (Exception throwException)
+            {
+                Console.WriteLine(throwException.Message);
+            }
+
+            Console.ReadKey();
         }
     }
 }
